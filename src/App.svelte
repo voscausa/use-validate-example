f74a6f53bc01a178e90c5d376b82a26047ac4f7d
voscausa <robert.vos@gmail.com>
2021-11-07 13:30:50 +0100

    1.0.7 use validation config update 1.1.8

--- src/App.svelte
+++ src/App.svelte
@@ -32,14 +32,14 @@
     month: ["required", { range: { min: 1, max: 12 } }],
     name: [
       "required",
-      { len: { operator: ">=", len: 3, msg: "length must be at least 3 characters" } },
+      { len: { operator: ">=", len: 5, msg: "length must be at least 5 characters" } },
     ],
     experience: "required",
     html: "get", // if html we require css
     css: "get", // get bool
-    js: "jsSkills", // if js we rquire jsSkills
-    jsSkills: "required",
-    other: "get", // get but not required
+    jsSkills: "get",
+    js: "updateJsSkillRules", // if js we require jsSkills
+    other: { wordCount: { count: 5, msg: "include at least 5 other skills" } },
   };
 
   // reset optional fields
@@ -48,8 +48,9 @@
 
   const notValidMarkers = {}; // not used
   // initialize the validation instance with node.name as the default id
-  const { field, OK, addValidator, fieldValues, runRuleChain } = validate(
-    { rulesConfig },
+  const { field, OK, addValidator, fieldValues, runRuleChain, setNotValid } = validate(
+    // markDefault 0: no-border and no-text, 1: red-border 2: text 3: red-border and text
+    { rulesConfig, lazy: true, markDefault: 3, alertBelow: 0 },
     // callback not used
     (id, notValid, value) => {
       // callback to update bindings or signal notValid components
@@ -57,12 +58,20 @@
     }
   );
 
-  // alt rulechain selection to validate optional required jsSkills
-  addValidator("jsSkills", function () {
-    runRuleChain.jsSkills(
-      // require skill for js
-      this.value ? "required" : "get"
-    );
+  // add a basic validator with setNotValid marker
+  addValidator("wordCount", function (obj) {
+    let { count = 2, msg = `number of words min ${count}` } = obj;
+    const valid = this.value.split(" ").length >= count;
+    return setNotValid(this, !valid, msg);
+  });
+
+  // add a validator which updates jsSkill rules based on js bool
+  addValidator("updateJsSkillRules", function () {
+    if ("jsSkills" in runRuleChain)
+      runRuleChain.jsSkills(
+        // require skill for js
+        this.value ? "required" : "get"
+      );
     // this section validator is always OK, so return false
     return false;
   });
